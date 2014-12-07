4951ebed770ffbe392b943646d6c9ac3309b1f1a
Connor Peet <connor@peet.io>
2014-12-07 21:56:33 +0000

    Initial of skew functionality. It compiles!

--- transform.go
+++ transform.go
@@ -2,7 +2,12 @@ package imaging
 
 import (
 	"image"
+	"image/color"
+	"math"
 )
+
+// Small number that we look at to compare floating points.
+const EPSILON = 0.0000001
 
 // Rotate90 rotates the image 90 degrees counterclockwise and returns the transformed image.
 func Rotate90(img image.Image) *image.NRGBA {
@@ -142,4 +147,79 @@ func FlipV(img image.Image) *image.NRGBA {
 	})
 
 	return dst
+}
+
+// Scews the image around its center, adjusting for the new max width/height
+func Skew(img image.Image, xOffset, yOffset int) *image.NRGBA {
+	src := toNRGBA(img)
+	srcW := src.Bounds().Max.X
+	srcH := src.Bounds().Max.Y
+	dstW := srcW + xOffset
+	dstH := srcH + yOffset
+	dst := image.NewNRGBA(image.Rect(0, 0, dstW, dstH))
+
+	srcHalfW := float64(srcW) / 2
+	srcHalfH := float64(srcH) / 2
+	dstHalfW := float64(dstW) / 2
+	dstHalfH := float64(dstH) / 2
+	xOffsetFloat := float64(xOffset)
+	yOffsetFloat := float64(yOffset)
+
+	parallel(dstW*dstH, func(partStart, partEnd int) {
+		for dstPixel := partStart; dstPixel < partEnd; dstPixel++ {
+			// Desination coordinates on the skewed image
+			dstY := float64(dstPixel / dstH) // Integer division
+			dstX := float64(dstPixel - (dstPixel % dstH))
+
+			// The change at the position caused by the screw
+			xDelta := (dstX/dstHalfW - 1) * xOffsetFloat
+			yDelta := (dstY/dstHalfH - 1) * yOffsetFloat
+
+			// Based on that, get the destination positions
+			srcX := int(srcHalfW + xDelta)
+			srcY := int(srcHalfH + yDelta)
+
+			offset := src.PixOffset(int(srcX), int(srcY))
+			aliasedAdd(dst, dstX, dstY, color.NRGBA{
+				R: src.Pix[offset+0],
+				G: src.Pix[offset+1],
+				B: src.Pix[offset+2],
+				A: src.Pix[offset+3],
+			})
+		}
+	})
+
+	return dst
+}
+
+// Adds the color on the image in a bicubic fashion.
+func aliasedAdd(img *image.NRGBA, x, y float64, col color.NRGBA) {
+	aliasedAddPixel(img, x, y, int(x), int(y), col)
+	aliasedAddPixel(img, x, y, int(x), int(y)+1, col)
+	aliasedAddPixel(img, x, y, int(x)+1, int(y), col)
+	aliasedAddPixel(img, x, y, int(x)+1, int(y)+1, col)
+}
+
+func aliasedAddPixel(img *image.NRGBA, x, y float64, px, py int, col color.NRGBA) {
+	offset := img.PixOffset(px, py)
+	opacity := float64(255*col.A) * (math.Pow(float64(px)-x, 2) + math.Pow(float64(py)-y, 2))
+
+	if opacity < 0 {
+		return
+	}
+
+	if img.Pix[offset+3] == 0 {
+		// If this pixel has *no* color yet (alpha = 0), set it exactly
+		img.Pix[offset+0] = col.R
+		img.Pix[offset+1] = col.G
+		img.Pix[offset+2] = col.B
+	} else {
+		// Otherwise mix it in
+		img.Pix[offset+0] = (col.R + img.Pix[offset+0]) / 2
+		img.Pix[offset+1] = (col.G + img.Pix[offset+1]) / 2
+		img.Pix[offset+3] = (col.B + img.Pix[offset+3]) / 2
+	}
+
+	// Finally adjust the opacity
+	img.Pix[offset+3] += uint8(opacity)
 }
